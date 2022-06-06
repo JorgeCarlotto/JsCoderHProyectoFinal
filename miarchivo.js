eaabd28a21ab5c0f575166d665cd462e23714450
jorge carlotto <jorgemcarlotto@gmail.com>
2022-06-06 18:08:18 -0300

    funcion distance test

--- miarchivo.js
+++ miarchivo.js
@@ -1,34 +1,68 @@
 
-alert("Bienvenido Piloto")
+alert("Bienvenido Piloto a nuestra sección Distancia entre Puntos")
 
 //CAPTURA DE DATOS POR PARTE DEL USUARIO
 
-//Primero capturo los datos con un pront, la idea es diseñar un formulario para cargar datos.
+//Primero capturo los datos con un pront, la idea es en el fututo diseñar un formulario para cargar datos.
 alert("Primero vamos a seleccionar la posición de donde partimos")
 
-//Datos Latitud punto de partida
+let userChoose = prompt("Que preferis elegir (1)Aeropuerto o (2)Cargar mi Latitud y Longitud")
+
+let inicio = asd(userChoose)
+
+function asd ( numero ){
+
+    if(parseInt(numero)=== 1){
+        
+    alert("Aeropuerto de salida")
+        let aeropuertoSalida =  prompt('Mar del Plata,Aeroparque,Ezeiza')
+
+    alert("Aeropuerto de llegada")
+        let aeropuertoLlegada =  prompt('Mar del Plata,Aeroparque,Ezeiza')
+
+    alert("Por el momento la funcion no se encuentra disponible intente cargar Latitud y Longitud")
+
+    }else if(parseInt(numero) === 2){
+
+ alert("Ahora vamos a seleccionar la posición de donde salimos")
+ alert("Ejemplo : 42° 00' 00''")
+        //Datos Latitud punto de partida
 let latGraUno = prompt("Latitud : Ingresa los grados")
 let latMinuUno = prompt("Latitud : Ingresa los minutos")
 let latSegUno = prompt("Latitud : Ingresa los segundos")
 
 //Datos Longitud punto de partida
-
+alert("Ejemplo : 58° 00' 00''")
 let longGraUno = prompt("Longitud : Ingresa los grados")
 let longMinuUno = prompt("Longitud : Ingresa los minutos")
 let longSegUno = prompt("Longitud : Ingresa los segundos")
 
 alert("Ahora vamos a seleccionar la posición a donde queremos ir")
-
+alert("Ejemplo : 41° 00' 00''")
 //Datos Latitud punto de LLegada
 let latGraDos = prompt("Latitud : Ingresa los grados")
 let latMinuDos = prompt("Latitud : Ingresa los minutos")
 let latSegDos = prompt("Latitud : Ingresa los segundos")
 
 //Datos Longitud punto de LLegada
-
+alert("Ejemplo : 58° 00' 00''")
 let longGraDos = prompt("Longitud : Ingresa los grados")
 let longMinuDos = prompt("Longitud : Ingresa los minutos")
 let longSegDos = prompt("Longitud : Ingresa los segundos")
+
+let lat1 = LatLogGradosDecimales (latGraUno,latMinuUno,latSegUno)
+let lon1 = LatLogGradosDecimales (longGraUno,longMinuUno,longSegUno)
+let lat2 = LatLogGradosDecimales (latGraDos,latMinuDos,latSegDos)
+let lon2 = LatLogGradosDecimales (longGraDos,longMinuDos,longSegDos)
+
+alert("La distancia a tu destino es de: "+ (Dist(lat1, lon1, lat2, lon2))+"millas nauticas")
+
+    }else{
+        alert('El dato seleccionado no es valido')
+    }
+}
+
+
 
 
 //CALCULOS DE POSICIONAMIENTO Y DISTANCIA.
@@ -39,13 +73,6 @@ function LatLogGradosDecimales (grados,minutos,segundos){
     let result = (parseInt(grados)+(parseInt(minutos)/60)+(parseInt(segundos)/60))*-1;
     return result;
 }
-
-
-let lat1 = LatLogGradosDecimales (latGraUno,latMinuUno,latSegUno)
-let lon1 = LatLogGradosDecimales (longGraUno,longMinuUno,longSegUno)
-let lat2 = LatLogGradosDecimales (latGraDos,latMinuDos,latSegDos)
-let lon2 = LatLogGradosDecimales (longGraDos,longMinuDos,longSegDos)
-
 
 
 //Con los datos pasados a grados decimales se aplica la siguiente formula 
@@ -68,4 +95,3 @@ function Dist(lat1, lon1, lat2, lon2) {
 
 
 
-alert("La distancia a tu destino es de: "+ (Dist(lat1, lon1, lat2, lon2))+"millas nauticas")
