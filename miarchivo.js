d76284ad1453f36ca6c1efe9a8a527b0d15ba4ed
jorge carlotto <jorgemcarlotto@gmail.com>
2022-06-20 20:13:57 -0300

    onclick en herramientas ok

--- miarchivo.js
+++ miarchivo.js
@@ -5,269 +5,256 @@
 //Primero se debe cambiar del formato grados minutos y segundos a Grados decimales.
 
 function LatLogGradosDecimales(grados, minutos, segundos) {
-  let result =
-    (parseInt(grados) + parseInt(minutos) / 60 + parseInt(segundos) / 60) * -1;
-  return result;
-}
-
-//Con los datos pasados a grados decimales se aplica la siguiente f
-
-function Dist(lat1, lon1, lat2, lon2) {
-  rad = function (x) {
-    return (x * Math.PI) / 180;
-  };
-
-  let R = 3443.918; //Radio de la tierra en km 6378.137  //Radio de la tierra en nm 3443.918
-  let dLat = rad(lat2 - lat1);
-  let dLong = rad(lon2 - lon1);
-  let a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(rad(lat1)) *
-      Math.cos(rad(lat2)) *
-      Math.sin(dLong / 2) *
-      Math.sin(dLong / 2);
-  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  let d = R * c;
-  return d.toFixed(1); //Retorna la distancia entre los dos puntos.
-}
-
-
-// Inicio de APP
-
-alert("Bienvenido Piloto a nuestra App Aeronautica");
-
-//CAPTURA DE DATOS POR PARTE DEL USUARIO
-
-//Primero capturo los datos con un pront, la idea es en el fututo diseñar un formulario para cargar y validar datos.
-
-alert("Primero vamos a seleccionar que queremos hacer");
-
-alert(
-"Selecciona la opcion con el numero correspondiente. Ej: (3)Precio de Combustible, escribir el numero 3 en la pantalla"
-);
-
-let userChoose = prompt(
-"Que preferis elegir distancia desde  (1)Aeropuertos o (2)Cargar mi Latitud y Longitud, (3)Precio de Combustible, (4)Crear Nuestro Avión"
-);
-
-let inicio = distance(userChoose);
-
-function distance(number) {
-if (parseInt(number) === 1) {
-  alert("Aeropuerto de salida");
-  let aeropuertoSalida = prompt("(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza");
-
-  alert("Aeropuerto de llegada");
-  let aeropuertoLlegada = prompt("(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza");
-
-  //Objetos simulan base de datos de aeropuertos
-  let sazm = {
-    id: 1,
-    nombre: "Mar del Plata",
-    lat: [37, 56, 02],
-    log: [57, 34, 22],
-  };
-
-  let sabe = {
-    id: 2,
-    nombre: "Aeroparque",
-    lat: [34, 33, 32],
-    log: [58, 24, 57],
-  };
-
-  let saez = {
-    id: 3,
-    nombre: "Ezeiza",
-    lat: [34, 49, 19],
-    log: [58, 32, 09],
-  };
-
-  let dataBaseAirpot = [sazm, sabe, saez];
-
-
-  // recorre la base compara la selección gerenera arry con las posiciones con formato editado
-  function takeoffAirport(takeoff) {
-    for (let i = 0; i < dataBaseAirpot.length; i++) {
-      if (parseInt(takeoff) === dataBaseAirpot[i].id) {
-        let positionOne = [];
-        let lat1 = LatLogGradosDecimales(
-          dataBaseAirpot[i].lat[0],
-          dataBaseAirpot[i].lat[1],
-          dataBaseAirpot[i].lat[2]
-        );
-        let lon1 = LatLogGradosDecimales(
-          dataBaseAirpot[i].log[0],
-          dataBaseAirpot[i].log[1],
-          dataBaseAirpot[i].log[2]
-        );
-
-        positionOne.push(lat1, lon1);
-        return positionOne;
-      }
-    }
+    let result =
+      (parseInt(grados) + parseInt(minutos) / 60 + parseInt(segundos) / 60) * -1;
+    return result;
   }
-
-  function landingAirport(landing) {
-    for (let i = 0; i < dataBaseAirpot.length; i++) {
-      if (parseInt(landing) === dataBaseAirpot[i].id) {
-        let positionTwo = [];
-        let lat1 = LatLogGradosDecimales(
-          dataBaseAirpot[i].lat[0],
-          dataBaseAirpot[i].lat[1],
-          dataBaseAirpot[i].lat[2]
-        );
-        let lon1 = LatLogGradosDecimales(
-          dataBaseAirpot[i].log[0],
-          dataBaseAirpot[i].log[1],
-          dataBaseAirpot[i].log[2]
-        );
-
-        positionTwo.push(lat1, lon1);
-        return positionTwo;
-      }
-    }
+  
+  //Con los datos pasados a grados decimales se aplica la siguiente f
+  
+  function Dist(lat1, lon1, lat2, lon2) {
+    rad = function (x) {
+      return (x * Math.PI) / 180;
+    };
+  
+    let R = 3443.918; //Radio de la tierra en km 6378.137  //Radio de la tierra en nm 3443.918
+    let dLat = rad(lat2 - lat1);
+    let dLong = rad(lon2 - lon1);
+    let a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(rad(lat1)) *
+        Math.cos(rad(lat2)) *
+        Math.sin(dLong / 2) *
+        Math.sin(dLong / 2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = R * c;
+    return d.toFixed(1); //Retorna la distancia entre los dos puntos.
   }
-
-  let toaLat = takeoffAirport(aeropuertoSalida)[0];
-  let toaLong = takeoffAirport(aeropuertoSalida)[1];
-  let lanLat = landingAirport(aeropuertoLlegada)[0];
-  let lanlog = landingAirport(aeropuertoLlegada)[1];
-
-  alert(
-    "La distancia entre sus dos aeropuertos es: " +
-      Dist(toaLat, toaLong, lanLat, lanlog)
-  );
-} else if (parseInt(number) === 2) {
-  alert("Ahora vamos a seleccionar la posición de donde salimos");
-  alert("Ejemplo : 42° 00' 00''");
-  //Datos Latitud punto de partida
-  let latGraUno = prompt("Latitud : Ingresa los grados");
-  let latMinuUno = prompt("Latitud : Ingresa los minutos");
-  let latSegUno = prompt("Latitud : Ingresa los segundos");
-
-  //Datos Longitud punto de partida
-  alert("Ejemplo : 58° 00' 00''");
-  let longGraUno = prompt("Longitud : Ingresa los grados");
-  let longMinuUno = prompt("Longitud : Ingresa los minutos");
-  let longSegUno = prompt("Longitud : Ingresa los segundos");
-
-  alert("Ahora vamos a seleccionar la posición a donde queremos ir");
-  alert("Ejemplo : 41° 00' 00''");
-  //Datos Latitud punto de LLegada
-  let latGraDos = prompt("Latitud : Ingresa los grados");
-  let latMinuDos = prompt("Latitud : Ingresa los minutos");
-  let latSegDos = prompt("Latitud : Ingresa los segundos");
-
-  //Datos Longitud punto de LLegada
-  alert("Ejemplo : 58° 00' 00''");
-  let longGraDos = prompt("Longitud : Ingresa los grados");
-  let longMinuDos = prompt("Longitud : Ingresa los minutos");
-  let longSegDos = prompt("Longitud : Ingresa los segundos");
-
-  let lat1 = LatLogGradosDecimales(latGraUno, latMinuUno, latSegUno);
-  let lon1 = LatLogGradosDecimales(longGraUno, longMinuUno, longSegUno);
-  let lat2 = LatLogGradosDecimales(latGraDos, latMinuDos, latSegDos);
-  let lon2 = LatLogGradosDecimales(longGraDos, longMinuDos, longSegDos);
-
-  alert(
-    "La distancia a tu destino es de: " +
-      Dist(lat1, lon1, lat2, lon2) +
-      "millas nauticas"
-  );
-} else if (parseInt(number) === 3) {
-
-  alert(
-    "En este momento tenemos disponibles en nuestra base de datos estos tres Aeropuertos"
-  );
-
-  //Objetos simulan base de datos Plantas de combustible
-  let marDelPlata = {
-    id: 1,
-    nombre: " Planta YPF Aeropuerto Mar del Plata ",
-    aeroNafta: 190,
-    JetA1: 150,
-  };
-
-  let ezeiza = {
-    id: 2,
-    nombre: "Planta SHELL Aeropuerto Ezeiza",
-    aeroNafta: 170,
-    JetA1: 130,
-  };
-
-  let comodoroRivadavia = {
-    id: 3,
-    nombre: "Planta YPF Aeropuerto Comodoro Rivadavia",
-    aeroNafta: 120,
-    JetA1: 100,
-  };
-
-  let aeropuertos = [marDelPlata, ezeiza, comodoroRivadavia];
-
-  for (let i = 0; i < aeropuertos.length; i++) {
-    alert(
-      aeropuertos[i].nombre +
-        " --- " +
-        "Precio de Aereo nafta: " +
-        aeropuertos[i].aeroNafta +
-        "$" +
-        "--- " +
-        "Precio de JetA1:" +
-        aeropuertos[i].JetA1 +
-        "$"
-    );
-  }
-} else if (parseInt(number) === 4) {
-
-  alert(
-    "Con Esta Función vamos a crear Nuestro Avion"
-  );
-   
-
-  let ModeloDeAvion= prompt("Ingresa el Modelo de tu Avión");
-  let velocidadCrucero= prompt("Ingresa velocidad de crucero");
-  let cantidadMaximaCombustible= prompt("Ingresa la Cantidad Maxima de Carga de combustible");
-
-
-  function Avion (modelo,vcrucero,combuMax){
-    this.modelo = modelo;
-    this.vcrucero =vcrucero;
-    this.combuMax =combuMax;
-  };
-
-  let avion = new Avion(ModeloDeAvion,velocidadCrucero,cantidadMaximaCombustible)
-
-  alert("Tu avion : " + avion.modelo + " Fue cargado de forma Exitosa")
   
 
-  alert(cargarAviones(avion))
+//----------------------------------------------------------------------------------------------
 
-  function cargarAviones(avionCreado,){
+  document.getElementById("disAeropuertos").onclick = function(){
 
-    let avionesEnDataBase =[
-      {modelo:"CESNA 150",
-      vcrucero:75,
-      combuMax:100
-    },
-    {modelo:"PIPER WARRIOR 3",
-    vcrucero:105,
-    combuMax:150
-  },
-    ];
+    alert("Aeropuerto de salida");
+    let aeropuertoSalida = prompt("(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza");
+    
+    alert("Aeropuerto de llegada");
+    let aeropuertoLlegada = prompt("(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza");
+    
+    //Objetos simulan base de datos de aeropuertos
+    let sazm = {
+      id: 1,
+      nombre: "Mar del Plata",
+      lat: [37, 56, 02],
+      log: [57, 34, 22],
+    };
+    
+    let sabe = {
+      id: 2,
+      nombre: "Aeroparque",
+      lat: [34, 33, 32],
+      log: [58, 24, 57],
+    };
+    
+    let saez = {
+      id: 3,
+      nombre: "Ezeiza",
+      lat: [34, 49, 19],
+      log: [58, 32, 09],
+    };
+    
+    let dataBaseAirpot = [sazm, sabe, saez];
+    
+    
+    // recorre la base compara la selección gerenera arry con las posiciones con formato editado
+    function takeoffAirport(takeoff) {
+      for (let i = 0; i < dataBaseAirpot.length; i++) {
+        if (parseInt(takeoff) === dataBaseAirpot[i].id) {
+          let positionOne = [];
+          let lat1 = LatLogGradosDecimales(
+            dataBaseAirpot[i].lat[0],
+            dataBaseAirpot[i].lat[1],
+            dataBaseAirpot[i].lat[2]
+          );
+          let lon1 = LatLogGradosDecimales(
+            dataBaseAirpot[i].log[0],
+            dataBaseAirpot[i].log[1],
+            dataBaseAirpot[i].log[2]
+          );
+    
+          positionOne.push(lat1, lon1);
+          return positionOne;
+        }
+      }
+    }
+    
+    function landingAirport(landing) {
+      for (let i = 0; i < dataBaseAirpot.length; i++) {
+        if (parseInt(landing) === dataBaseAirpot[i].id) {
+          let positionTwo = [];
+          let lat1 = LatLogGradosDecimales(
+            dataBaseAirpot[i].lat[0],
+            dataBaseAirpot[i].lat[1],
+            dataBaseAirpot[i].lat[2]
+          );
+          let lon1 = LatLogGradosDecimales(
+            dataBaseAirpot[i].log[0],
+            dataBaseAirpot[i].log[1],
+            dataBaseAirpot[i].log[2]
+          );
+    
+          positionTwo.push(lat1, lon1);
+          return positionTwo;
+        }
+      }
+    }
+    
+    let toaLat = takeoffAirport(aeropuertoSalida)[0];
+    let toaLong = takeoffAirport(aeropuertoSalida)[1];
+    let lanLat = landingAirport(aeropuertoLlegada)[0];
+    let lanlog = landingAirport(aeropuertoLlegada)[1];
+    
+    alert(
+      "La distancia entre sus dos aeropuertos es: " +
+        Dist(toaLat, toaLong, lanLat, lanlog)
+    );
+      }
 
-    let listaAviones = avionesEnDataBase.push(avionCreado);
 
-   
-    let recorridoAviones = avionesEnDataBase.map(function(e){
-      return e.modelo
-    })
 
-    return recorridoAviones
+  document.getElementById("calculosPuntos").onclick = function(){
+
+    alert("Ahora vamos a seleccionar la posición de donde salimos");
+    alert("Ejemplo : 42° 00' 00''");
+    //Datos Latitud punto de partida
+    let latGraUno = prompt("Latitud : Ingresa los grados");
+    let latMinuUno = prompt("Latitud : Ingresa los minutos");
+    let latSegUno = prompt("Latitud : Ingresa los segundos");
+  
+    //Datos Longitud punto de partida
+    alert("Ejemplo : 58° 00' 00''");
+    let longGraUno = prompt("Longitud : Ingresa los grados");
+    let longMinuUno = prompt("Longitud : Ingresa los minutos");
+    let longSegUno = prompt("Longitud : Ingresa los segundos");
+  
+    alert("Ahora vamos a seleccionar la posición a donde queremos ir");
+    alert("Ejemplo : 41° 00' 00''");
+    //Datos Latitud punto de LLegada
+    let latGraDos = prompt("Latitud : Ingresa los grados");
+    let latMinuDos = prompt("Latitud : Ingresa los minutos");
+    let latSegDos = prompt("Latitud : Ingresa los segundos");
+  
+    //Datos Longitud punto de LLegada
+    alert("Ejemplo : 58° 00' 00''");
+    let longGraDos = prompt("Longitud : Ingresa los grados");
+    let longMinuDos = prompt("Longitud : Ingresa los minutos");
+    let longSegDos = prompt("Longitud : Ingresa los segundos");
+  
+    let lat1 = LatLogGradosDecimales(latGraUno, latMinuUno, latSegUno);
+    let lon1 = LatLogGradosDecimales(longGraUno, longMinuUno, longSegUno);
+    let lat2 = LatLogGradosDecimales(latGraDos, latMinuDos, latSegDos);
+    let lon2 = LatLogGradosDecimales(longGraDos, longMinuDos, longSegDos);
+  
+    alert(
+      "La distancia a tu destino es de: " +
+        Dist(lat1, lon1, lat2, lon2) +
+        "millas nauticas"
+    );
+
+  }     
+
+
+  document.getElementById("calculosCumbustible").onclick = function(){
+
+    alert(
+        "En este momento tenemos disponibles en nuestra base de datos estos tres Aeropuertos"
+      );
+    
+      //Objetos simulan base de datos Plantas de combustible
+      let marDelPlata = {
+        id: 1,
+        nombre: " Planta YPF Aeropuerto Mar del Plata ",
+        aeroNafta: 190,
+        JetA1: 150,
+      };
+    
+      let ezeiza = {
+        id: 2,
+        nombre: "Planta SHELL Aeropuerto Ezeiza",
+        aeroNafta: 170,
+        JetA1: 130,
+      };
+    
+      let comodoroRivadavia = {
+        id: 3,
+        nombre: "Planta YPF Aeropuerto Comodoro Rivadavia",
+        aeroNafta: 120,
+        JetA1: 100,
+      };
+    
+      let aeropuertos = [marDelPlata, ezeiza, comodoroRivadavia];
+    
+      for (let i = 0; i < aeropuertos.length; i++) {
+        alert(
+          aeropuertos[i].nombre +
+            " --- " +
+            "Precio de Aereo nafta: " +
+            aeropuertos[i].aeroNafta +
+            "$" +
+            "--- " +
+            "Precio de JetA1:" +
+            aeropuertos[i].JetA1 +
+            "$"
+        );
+      }
+  
   }
 
+// FUNICION CREAR AVION
+
+//   alert(
+//     "Con Esta Función vamos a crear Nuestro Avion"
+//   );
+   
+
+//   let ModeloDeAvion= prompt("Ingresa el Modelo de tu Avión");
+//   let velocidadCrucero= prompt("Ingresa velocidad de crucero");
+//   let cantidadMaximaCombustible= prompt("Ingresa la Cantidad Maxima de Carga de combustible");
 
 
-   } else {
-  alert("El dato seleccionado no es valido");
-}
-}
+//   function Avion (modelo,vcrucero,combuMax){
+//     this.modelo = modelo;
+//     this.vcrucero =vcrucero;
+//     this.combuMax =combuMax;
+//   };
+
+//   let avion = new Avion(ModeloDeAvion,velocidadCrucero,cantidadMaximaCombustible)
+
+//   alert("Tu avion : " + avion.modelo + " Fue cargado de forma Exitosa")
+  
+
+//   alert(cargarAviones(avion))
+
+//   function cargarAviones(avionCreado,){
+
+//     let avionesEnDataBase =[
+//       {modelo:"CESNA 150",
+//       vcrucero:75,
+//       combuMax:100
+//     },
+//     {modelo:"PIPER WARRIOR 3",
+//     vcrucero:105,
+//     combuMax:150
+//   },
+//     ];
+
+//     let listaAviones = avionesEnDataBase.push(avionCreado);
+
+   
+//     let recorridoAviones = avionesEnDataBase.map(function(e){
+//       return e.modelo
+//     })
+
+//     return recorridoAviones
+//   }
