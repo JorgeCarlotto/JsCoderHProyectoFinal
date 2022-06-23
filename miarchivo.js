b715e0ca0542c7aca32ed42609612cdf3d3da145
Estación Aérea Mar del Plata EAMP <jorgemcarlotto@gmail.com>
2022-06-23 18:15:11 -0300

    Agregando eventos

--- miarchivo.js
+++ miarchivo.js
@@ -34,92 +34,92 @@ function LatLogGradosDecimales(grados, minutos, segundos) {
 
 //----------------------------------------------------------------------------------------------
 
-  document.getElementById("disAeropuertos").onclick = function(){
+  // document.getElementById("disAeropuertos").onclick = function(){
 
-    alert("Aeropuerto de salida");
-    let aeropuertoSalida = prompt("(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza");
+  //   alert("Aeropuerto de salida");
+  //   let aeropuertoSalida = prompt("(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza");
     
-    alert("Aeropuerto de llegada");
-    let aeropuertoLlegada = prompt("(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza");
+  //   alert("Aeropuerto de llegada");
+  //   let aeropuertoLlegada = prompt("(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza");
     
-    //Objetos simulan base de datos de aeropuertos
-    let sazm = {
-      id: 1,
-      nombre: "Mar del Plata",
-      lat: [37, 56, 02],
-      log: [57, 34, 22],
-    };
+  //   //Objetos simulan base de datos de aeropuertos
+  //   let sazm = {
+  //     id: 1,
+  //     nombre: "Mar del Plata",
+  //     lat: [37, 56, 02],
+  //     log: [57, 34, 22],
+  //   };
     
-    let sabe = {
-      id: 2,
-      nombre: "Aeroparque",
-      lat: [34, 33, 32],
-      log: [58, 24, 57],
-    };
+  //   let sabe = {
+  //     id: 2,
+  //     nombre: "Aeroparque",
+  //     lat: [34, 33, 32],
+  //     log: [58, 24, 57],
+  //   };
     
-    let saez = {
-      id: 3,
-      nombre: "Ezeiza",
-      lat: [34, 49, 19],
-      log: [58, 32, 09],
-    };
+  //   let saez = {
+  //     id: 3,
+  //     nombre: "Ezeiza",
+  //     lat: [34, 49, 19],
+  //     log: [58, 32, 09],
+  //   };
     
-    let dataBaseAirpot = [sazm, sabe, saez];
+  //   let dataBaseAirpot = [sazm, sabe, saez];
     
     
-    // recorre la base compara la selección gerenera arry con las posiciones con formato editado
-    function takeoffAirport(takeoff) {
-      for (let i = 0; i < dataBaseAirpot.length; i++) {
-        if (parseInt(takeoff) === dataBaseAirpot[i].id) {
-          let positionOne = [];
-          let lat1 = LatLogGradosDecimales(
-            dataBaseAirpot[i].lat[0],
-            dataBaseAirpot[i].lat[1],
-            dataBaseAirpot[i].lat[2]
-          );
-          let lon1 = LatLogGradosDecimales(
-            dataBaseAirpot[i].log[0],
-            dataBaseAirpot[i].log[1],
-            dataBaseAirpot[i].log[2]
-          );
+  //   // recorre la base compara la selección gerenera arry con las posiciones con formato editado
+  //   function takeoffAirport(takeoff) {
+  //     for (let i = 0; i < dataBaseAirpot.length; i++) {
+  //       if (parseInt(takeoff) === dataBaseAirpot[i].id) {
+  //         let positionOne = [];
+  //         let lat1 = LatLogGradosDecimales(
+  //           dataBaseAirpot[i].lat[0],
+  //           dataBaseAirpot[i].lat[1],
+  //           dataBaseAirpot[i].lat[2]
+  //         );
+  //         let lon1 = LatLogGradosDecimales(
+  //           dataBaseAirpot[i].log[0],
+  //           dataBaseAirpot[i].log[1],
+  //           dataBaseAirpot[i].log[2]
+  //         );
     
-          positionOne.push(lat1, lon1);
-          return positionOne;
-        }
-      }
-    }
+  //         positionOne.push(lat1, lon1);
+  //         return positionOne;
+  //       }
+  //     }
+  //   }
     
-    function landingAirport(landing) {
-      for (let i = 0; i < dataBaseAirpot.length; i++) {
-        if (parseInt(landing) === dataBaseAirpot[i].id) {
-          let positionTwo = [];
-          let lat1 = LatLogGradosDecimales(
-            dataBaseAirpot[i].lat[0],
-            dataBaseAirpot[i].lat[1],
-            dataBaseAirpot[i].lat[2]
-          );
-          let lon1 = LatLogGradosDecimales(
-            dataBaseAirpot[i].log[0],
-            dataBaseAirpot[i].log[1],
-            dataBaseAirpot[i].log[2]
-          );
+  //   function landingAirport(landing) {
+  //     for (let i = 0; i < dataBaseAirpot.length; i++) {
+  //       if (parseInt(landing) === dataBaseAirpot[i].id) {
+  //         let positionTwo = [];
+  //         let lat1 = LatLogGradosDecimales(
+  //           dataBaseAirpot[i].lat[0],
+  //           dataBaseAirpot[i].lat[1],
+  //           dataBaseAirpot[i].lat[2]
+  //         );
+  //         let lon1 = LatLogGradosDecimales(
+  //           dataBaseAirpot[i].log[0],
+  //           dataBaseAirpot[i].log[1],
+  //           dataBaseAirpot[i].log[2]
+  //         );
     
-          positionTwo.push(lat1, lon1);
-          return positionTwo;
-        }
-      }
-    }
+  //         positionTwo.push(lat1, lon1);
+  //         return positionTwo;
+  //       }
+  //     }
+  //   }
     
-    let toaLat = takeoffAirport(aeropuertoSalida)[0];
-    let toaLong = takeoffAirport(aeropuertoSalida)[1];
-    let lanLat = landingAirport(aeropuertoLlegada)[0];
-    let lanlog = landingAirport(aeropuertoLlegada)[1];
+  //   let toaLat = takeoffAirport(aeropuertoSalida)[0];
+  //   let toaLong = takeoffAirport(aeropuertoSalida)[1];
+  //   let lanLat = landingAirport(aeropuertoLlegada)[0];
+  //   let lanlog = landingAirport(aeropuertoLlegada)[1];
     
-    alert(
-      "La distancia entre sus dos aeropuertos es: " +
-        Dist(toaLat, toaLong, lanLat, lanlog)
-    );
-      }
+  //   alert(
+  //     "La distancia entre sus dos aeropuertos es: " +
+  //       Dist(toaLat, toaLong, lanLat, lanlog)
+  //   );
+  //     }
 
 
 
@@ -167,6 +167,7 @@ function LatLogGradosDecimales(grados, minutos, segundos) {
 
   document.getElementById("calculosCumbustible").onclick = function(){
 
+    let div = document.createElement('div');
     alert(
         "En este momento tenemos disponibles en nuestra base de datos estos tres Aeropuertos"
       );
@@ -194,22 +195,118 @@ function LatLogGradosDecimales(grados, minutos, segundos) {
       };
     
       let aeropuertos = [marDelPlata, ezeiza, comodoroRivadavia];
-    
+      
+      
       for (let i = 0; i < aeropuertos.length; i++) {
-        alert(
-          aeropuertos[i].nombre +
-            " --- " +
-            "Precio de Aereo nafta: " +
-            aeropuertos[i].aeroNafta +
-            "$" +
-            "--- " +
-            "Precio de JetA1:" +
-            aeropuertos[i].JetA1 +
-            "$"
-        );
+        div.innerHTML +=
+            `
+<div class="container marketing">
+    <div class="row">
+      <div class="col-lg-4">
+        <h2 class="fw-normal">${aeropuertos[i].nombre}</h2>
+        <h3 class="fw-normal">Precio de Aero Nafta:</h3>
+        <p>${aeropuertos[i].aeroNafta} </p>
+        <h3 class="fw-normal">Precio de JetA1:</h3>
+        <p>${aeropuertos[i].JetA1} </p>
+      </div>
+    </div>
+    </div>
+    `
+
+
       }
+      contenedor.appendChild(div);
   
   }
+
+
+
+
+
+
+
+  const contenedor = document.querySelector('#dinamic');
+
+  const btnAgregar = document.getElementById('disAeropuertos2');
+
+  btnAgregar.addEventListener('click', e => {
+    let div = document.createElement('div');
+    div.innerHTML = `
+    <div class="b-example-divider"></div>
+
+    <div
+      class="modal modal-signin position-static d-block bg-secondary py-5"
+      tabindex="-1"
+      role="dialog"
+      id="modalSignin"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content rounded-4 shadow">
+          <div class="modal-header p-5 pb-4 border-bottom-0">
+            <!-- <h2 class="fw-bold mb-0">Distancia entre Aeropuertos</h2> -->
+
+            
+            <button
+            onclick="eliminar(this)" 
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+
+          <div class="modal-body p-5 pt-0">
+            <form id="formPosEntreAeropuertos">
+              <h3 class="fw-bold mb-0">Aeropuerto Salida</h3>
+              <div class="form-floating mb-3">
+                <input
+                  type="text"
+                  class="form-control rounded-3"
+                  id="floatingInput"
+                  placeholder="(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza"
+                />
+                <label for="floatingInput"
+                  >(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza</label
+                >
+              </div>
+              <h3 class="fw-bold mb-0">Aeropuerto Llegada</h3>
+              <div class="form-floating mb-3">
+                <input
+                  type="text"
+                  class="form-control rounded-3"
+                  id="floatingInput"
+                  placeholder="(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza"
+                />
+                <label for="floatingInput"
+                  >(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza</label
+                >
+              </div>
+              <button
+                class="w-100 mb-2 btn btn-lg rounded-3 btn-primary"
+                type="submit"
+              >
+                Calcular
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    contenedor.appendChild(div);
+})
+
+const eliminar = (e) => {
+  const divPadre = contenedor;
+  contenedor.remove(divPadre);
+  
+};
+
+
+
+
+
+
+
 
 // FUNICION CREAR AVION
 
