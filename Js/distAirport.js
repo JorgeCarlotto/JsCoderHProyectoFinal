233aeed55dfbcfd784a8cc64fe59f46ae37a9afc
Estación Aérea Mar del Plata EAMP <jorgemcarlotto@gmail.com>
2022-07-04 11:03:01 -0300

    optimizacion de proyecto

--- Js/distAirport.js
+++ Js/distAirport.js
@@ -1,127 +1,115 @@
 function LatLogGradosDecimales(grados, minutos, segundos) {
-    let result =
-      (parseInt(grados) + parseInt(minutos) / 60 + parseInt(segundos) / 60) * -1;
-    return result;
-  }
-  
-  //Con los datos pasados a grados decimales se aplica la siguiente f
-  
-  function Dist(lat1, lon1, lat2, lon2) {
-    rad = function (x) {
-      return (x * Math.PI) / 180;
-    };
-  
-    let R = 3443.918; //Radio de la tierra en km 6378.137  //Radio de la tierra en nm 3443.918
-    let dLat = rad(lat2 - lat1);
-    let dLong = rad(lon2 - lon1);
-    let a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(rad(lat1)) *
-        Math.cos(rad(lat2)) *
-        Math.sin(dLong / 2) *
-        Math.sin(dLong / 2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let d = R * c;
-    return d.toFixed(1); //Retorna la distancia entre los dos puntos.
-  }
+  let result =
+    (parseInt(grados) + parseInt(minutos) / 60 + parseInt(segundos) / 60) * -1;
+  return result;
+}
 
+//Con los datos pasados a grados decimales se aplica la siguiente f
 
+function Dist(lat1, lon1, lat2, lon2) {
+  rad = function (x) {
+    return (x * Math.PI) / 180;
+  };
 
+  let R = 3443.918; //Radio de la tierra en km 6378.137  //Radio de la tierra en nm 3443.918
+  let dLat = rad(lat2 - lat1);
+  let dLong = rad(lon2 - lon1);
+  let a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(lat1)) *
+      Math.cos(rad(lat2)) *
+      Math.sin(dLong / 2) *
+      Math.sin(dLong / 2);
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let d = R * c;
+  return d.toFixed(1); //Retorna la distancia entre los dos puntos.
+}
 
-
-
-
-
-const contenedor = document.querySelector('#resultadoDistAirport');
+const contenedor = document.querySelector("#resultadoDistAirport");
 let formDistAirpot = document.getElementById("DisAirportForm");
 
-formDistAirpot.addEventListener("submit",validarFormDisAirpot)
+formDistAirpot.addEventListener("submit", validarFormDisAirpot);
 
-function validarFormDisAirpot(e){
+function validarFormDisAirpot(e) {
+  e.preventDefault();
+  let div = document.createElement("div");
 
- e.preventDefault();
- let div = document.createElement('div');
+  let aeropuertoSalida = e.target[0].value;
+  let aeropuertoLlegada = e.target[1].value;
 
- let aeropuertoSalida = e.target[0].value
- let aeropuertoLlegada = e.target[1].value
+  //Objetos simulan base de datos de aeropuertos
+  let sazm = {
+    id: 1,
+    nombre: "Mar del Plata",
+    lat: [37, 56, 02],
+    log: [57, 34, 22],
+  };
 
+  let sabe = {
+    id: 2,
+    nombre: "Aeroparque",
+    lat: [34, 33, 32],
+    log: [58, 24, 57],
+  };
 
-   //Objetos simulan base de datos de aeropuertos
-   let sazm = {
-     id: 1,
-     nombre: "Mar del Plata",
-     lat: [37, 56, 02],
-     log: [57, 34, 22],
-   };
-   
-   let sabe = {
-     id: 2,
-     nombre: "Aeroparque",
-     lat: [34, 33, 32],
-     log: [58, 24, 57],
-   };
-   
-   let saez = {
-     id: 3,
-     nombre: "Ezeiza",
-     lat: [34, 49, 19],
-     log: [58, 32, 09],
-   };
-   
-   let dataBaseAirpot = [sazm, sabe, saez];
-   
-   
-   // recorre la base compara la selección gerenera arry con las posiciones con formato editado
-   function takeoffAirport(takeoff) {
-     for (let i = 0; i < dataBaseAirpot.length; i++) {
-       if (parseInt(takeoff) === dataBaseAirpot[i].id) {
-         let positionOne = [];
-         let lat1 = LatLogGradosDecimales(
-           dataBaseAirpot[i].lat[0],
-           dataBaseAirpot[i].lat[1],
-           dataBaseAirpot[i].lat[2]
-         );
-         let lon1 = LatLogGradosDecimales(
-           dataBaseAirpot[i].log[0],
-           dataBaseAirpot[i].log[1],
-           dataBaseAirpot[i].log[2]
-         );
-   
-         positionOne.push(lat1, lon1);
-         return positionOne;
-       }
-     }
-   }
-   
-   function landingAirport(landing) {
-     for (let i = 0; i < dataBaseAirpot.length; i++) {
-       if (parseInt(landing) === dataBaseAirpot[i].id) {
-         let positionTwo = [];
-         let lat1 = LatLogGradosDecimales(
-           dataBaseAirpot[i].lat[0],
-           dataBaseAirpot[i].lat[1],
-           dataBaseAirpot[i].lat[2]
-         );
-         let lon1 = LatLogGradosDecimales(
-           dataBaseAirpot[i].log[0],
-           dataBaseAirpot[i].log[1],
-           dataBaseAirpot[i].log[2]
-         );
-   
-         positionTwo.push(lat1, lon1);
-         return positionTwo;
-       }
-     }
-   }
-   
-   let toaLat = takeoffAirport(aeropuertoSalida)[0];
-   let toaLong = takeoffAirport(aeropuertoSalida)[1];
-   let lanLat = landingAirport(aeropuertoLlegada)[0];
-   let lanlog = landingAirport(aeropuertoLlegada)[1];
-   
+  let saez = {
+    id: 3,
+    nombre: "Ezeiza",
+    lat: [34, 49, 19],
+    log: [58, 32, 09],
+  };
 
+  let dataBaseAirpot = [sazm, sabe, saez];
 
-   div.innerHTML =`
+  // recorre la base compara la selección gerenera arry con las posiciones con formato editado
+  function takeoffAirport(takeoff) {
+    for (let i = 0; i < dataBaseAirpot.length; i++) {
+      if (parseInt(takeoff) === dataBaseAirpot[i].id) {
+        let positionOne = [];
+        let lat1 = LatLogGradosDecimales(
+          dataBaseAirpot[i].lat[0],
+          dataBaseAirpot[i].lat[1],
+          dataBaseAirpot[i].lat[2]
+        );
+        let lon1 = LatLogGradosDecimales(
+          dataBaseAirpot[i].log[0],
+          dataBaseAirpot[i].log[1],
+          dataBaseAirpot[i].log[2]
+        );
+
+        positionOne.push(lat1, lon1);
+        return positionOne;
+      }
+    }
+  }
+
+  function landingAirport(landing) {
+    for (let i = 0; i < dataBaseAirpot.length; i++) {
+      if (parseInt(landing) === dataBaseAirpot[i].id) {
+        let positionTwo = [];
+        let lat1 = LatLogGradosDecimales(
+          dataBaseAirpot[i].lat[0],
+          dataBaseAirpot[i].lat[1],
+          dataBaseAirpot[i].lat[2]
+        );
+        let lon1 = LatLogGradosDecimales(
+          dataBaseAirpot[i].log[0],
+          dataBaseAirpot[i].log[1],
+          dataBaseAirpot[i].log[2]
+        );
+
+        positionTwo.push(lat1, lon1);
+        return positionTwo;
+      }
+    }
+  }
+
+  let toaLat = takeoffAirport(aeropuertoSalida)[0];
+  let toaLong = takeoffAirport(aeropuertoSalida)[1];
+  let lanLat = landingAirport(aeropuertoLlegada)[0];
+  let lanlog = landingAirport(aeropuertoLlegada)[1];
+
+  div.innerHTML = `
    
    <div class="b-example-divider"></div>
    <div class="modal modal-sheet position-static d-block  py-5" tabindex="-1" role="dialog" id="modalSheet">
@@ -131,7 +119,12 @@ function validarFormDisAirpot(e){
              <h5 class="modal-title">La Distancia entre los aeropuertos es:</h5>         
            </div>
            <div class="modal-body py-0">
-             <h3 class="modal-title"> ${Dist(toaLat, toaLong, lanLat, lanlog)} Millas Nauticas</h3>
+             <h3 class="modal-title"> ${Dist(
+               toaLat,
+               toaLong,
+               lanLat,
+               lanlog
+             )} Millas Nauticas</h3>
            </div>
            <div class="modal-footer flex-column border-top-0">
              <button  onclick="eliminar(this)" type="button" class="btn btn-lg btn-danger w-100 mx-0" data-bs-dismiss="modal">Close</button>
@@ -141,86 +134,11 @@ function validarFormDisAirpot(e){
      </div>
    </div>;
    
-   `
-   contenedor.appendChild(div);  
-
+   `;
+  contenedor.appendChild(div);
 }
 
-
-
-//   const contenedor = document.querySelector('#dinamic');
-
-//   const btnAgregar = document.getElementById('disAeropuertos2');
-
-//   btnAgregar.addEventListener('click', e => {
-//     let div = document.createElement('div');
-//     div.innerHTML = `
-//     <div class="b-example-divider"></div>
-
-//     <div
-//       class="modal modal-signin position-static d-block bg-secondary py-5"
-//       tabindex="-1"
-//       role="dialog"
-//       id="modalSignin"
-//     >
-//       <div class="modal-dialog" role="document">
-//         <div class="modal-content rounded-4 shadow">
-//           <div class="modal-header p-5 pb-4 border-bottom-0">
-//             <!-- <h2 class="fw-bold mb-0">Distancia entre Aeropuertos</h2> -->
-
-            
-//             <button
-//             onclick="eliminar(this)" 
-//               type="button"
-//               class="btn-close"
-//               data-bs-dismiss="modal"
-//               aria-label="Close"
-//             ></button>
-//           </div>
-
-//           <div class="modal-body p-5 pt-0">
-//             <form id="formPosEntreAeropuertos">
-//               <h3 class="fw-bold mb-0">Aeropuerto Salida</h3>
-//               <div class="form-floating mb-3">
-//                 <input
-//                   type="text"
-//                   class="form-control rounded-3"
-//                   id="floatingInput"
-//                   placeholder="(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza"
-//                 />
-//                 <label for="floatingInput"
-//                   >(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza</label
-//                 >
-//               </div>
-//               <h3 class="fw-bold mb-0">Aeropuerto Llegada</h3>
-//               <div class="form-floating mb-3">
-//                 <input
-//                   type="text"
-//                   class="form-control rounded-3"
-//                   id="floatingInput"
-//                   placeholder="(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza"
-//                 />
-//                 <label for="floatingInput"
-//                   >(1)Mar del Plata,(2)Aeroparque,(3)Ezeiza</label
-//                 >
-//               </div>
-//               <button
-//                 class="w-100 mb-2 btn btn-lg rounded-3 btn-primary"
-//                 type="submit"
-//               >
-//                 Calcular
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </div>`;
-//     contenedor.appendChild(div);
-// })
-
 const eliminar = (e) => {
-    const divPadre = contenedor;
-    contenedor.remove(divPadre);
-    
-  };
-  
+  const divPadre = contenedor;
+  contenedor.remove(divPadre);
+};
--- Js/distanciaEntrePuntos.js
+++ Js/distanciaEntrePuntos.js
@@ -1,143 +1,137 @@
-//Funciones bases de la app 
+//Funciones bases de la app
 
 //CALCULOS DE POSICIONAMIENTO Y DISTANCIA.
 
 //Primero se debe cambiar del formato grados minutos y segundos a Grados decimales , Con la direccion determinamos el cuadrante cartografico.
 
-function decimal(grados, minutos, segundos, direccion)
-{
-if(direccion){
-signo     = (direccion.toLowerCase() == 'w' ||
-    direccion.toLowerCase() == 's') ?
-    -1 : 1;
-    direccion = (direccion.toLowerCase() == 'w' ||
-        direccion.toLowerCase() == 's' ||
-        direccion.toLowerCase() == 'n' ||
-        direccion.toLowerCase() == 'e') ?
-        direccion.toLowerCase() : '';
-    }
-    else{
-    signo     = (parseInt(grados) < 0) ? -1 : 1;
-    direccion = '';
-    }
-    dec = Math.round((Math.abs(parseInt(grados)) + ((parseInt(minutos) * 60) + parseInt(segundos)) / 3600) * 1000000) / 1000000;
-    if(isNaN(direccion) || direccion == '')
-        dec = dec * signo;
-
-    return dec 
-}
-  
-  //Con los datos pasados a grados decimales se aplica la siguiente f
-  
-  function Dist(lat1, lon1, lat2, lon2) {
-    rad = function (x) {
-      return (x * Math.PI) / 180;
-    };
-  
-    let R = 3443.918; //Radio de la tierra en km 6380.137  //Radio de la tierra en nm 3443.918
-    let dLat = rad(lat2 - lat1);
-    let dLong = rad(lon2 - lon1);
-    let a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(rad(lat1)) *
-        Math.cos(rad(lat2)) *
-        Math.sin(dLong / 2) *
-        Math.sin(dLong / 2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let d = R * c;
-    return d.toFixed(1); //Retorna la distancia entre los dos puntos.
+function decimal(grados, minutos, segundos, direccion) {
+  if (direccion) {
+    signo =
+      direccion.toLowerCase() == "w" || direccion.toLowerCase() == "s" ? -1 : 1;
+    direccion =
+      direccion.toLowerCase() == "w" ||
+      direccion.toLowerCase() == "s" ||
+      direccion.toLowerCase() == "n" ||
+      direccion.toLowerCase() == "e"
+        ? direccion.toLowerCase()
+        : "";
+  } else {
+    signo = parseInt(grados) < 0 ? -1 : 1;
+    direccion = "";
   }
+  dec =
+    Math.round(
+      (Math.abs(parseInt(grados)) +
+        (parseInt(minutos) * 60 + parseInt(segundos)) / 3600) *
+        1000000
+    ) / 1000000;
+  if (isNaN(direccion) || direccion == "") dec = dec * signo;
 
+  return dec;
+}
 
+//Con los datos pasados a grados decimales se aplica la siguiente f
 
-const postUnoLartText = document.querySelector('#postUnoLartText');
-const postUnoLongText = document.querySelector('#postUnoLongText');
-const postDosLartText = document.querySelector('#postDosLartText');
-const postDosLongText = document.querySelector('#postDosLongText');
+function Dist(lat1, lon1, lat2, lon2) {
+  rad = function (x) {
+    return (x * Math.PI) / 180;
+  };
 
-const verMapaPostUno = document.querySelector('#verMapaPostUno');
-const verMapaPostDos = document.querySelector('#verMapaPostDos');
+  let R = 3443.918; //Radio de la tierra en km 6380.137  //Radio de la tierra en nm 3443.918
+  let dLat = rad(lat2 - lat1);
+  let dLong = rad(lon2 - lon1);
+  let a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(lat1)) *
+      Math.cos(rad(lat2)) *
+      Math.sin(dLong / 2) *
+      Math.sin(dLong / 2);
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let d = R * c;
+  return d.toFixed(1); //Retorna la distancia entre los dos puntos.
+}
 
-const resultFinal = document.querySelector('#resultFinal');
+const postUnoLartText = document.querySelector("#postUnoLartText");
+const postUnoLongText = document.querySelector("#postUnoLongText");
+const postDosLartText = document.querySelector("#postDosLartText");
+const postDosLongText = document.querySelector("#postDosLongText");
 
+const verMapaPostUno = document.querySelector("#verMapaPostUno");
+const verMapaPostDos = document.querySelector("#verMapaPostDos");
+
+const resultFinal = document.querySelector("#resultFinal");
 
 let formDistPuntos = document.getElementById("formDistPuntos");
 
-formDistPuntos.addEventListener("submit",validarFormDistPuntos)
+formDistPuntos.addEventListener("submit", validarFormDistPuntos);
 
+function validarFormDistPuntos(e) {
+  e.preventDefault();
 
-function validarFormDistPuntos(e){
+  let smallLatUno = document.createElement("small");
+  let smallLongUno = document.createElement("small");
+  let smallLatDos = document.createElement("small");
+  let smallLongDos = document.createElement("small");
 
-    e.preventDefault();
+  let aPostUno = document.createElement("a");
+  let aPostDos = document.createElement("a");
 
-    let smallLatUno = document.createElement('small');
-    let smallLongUno = document.createElement('small');
-    let smallLatDos = document.createElement('small');
-    let smallLongDos = document.createElement('small');
+  let tituloCuatro = document.createElement("h4");
 
-    let aPostUno = document.createElement('a');
-    let aPostDos = document.createElement('a');
+  //Datos Latitud punto de partida
+  let latGraUno = e.target[0].value;
+  let latMinuUno = e.target[1].value;
+  let latSegUno = e.target[2].value;
 
-    let tituloCuatro =document.createElement('h4')
+  //Datos Longitud punto de partida
 
-    //Datos Latitud punto de partida
-    let latGraUno = e.target[0].value;
-    let latMinuUno = e.target[1].value;
-    let latSegUno = e.target[2].value;
-  
-    //Datos Longitud punto de partida
-    
-    let longGraUno = e.target[3].value;
-    let longMinuUno = e.target[4].value;
-    let longSegUno = e.target[5].value;
-  
- 
-    //Datos Latitud punto de LLegada
-    let latGraDos = e.target[6].value;
-    let latMinuDos =e.target[7].value;
-    let latSegDos = e.target[8].value;
-  
-    //Datos Longitud punto de LLegada
+  let longGraUno = e.target[3].value;
+  let longMinuUno = e.target[4].value;
+  let longSegUno = e.target[5].value;
 
-    let longGraDos = e.target[9].value;
-    let longMinuDos =e.target[10].value;
-    let longSegDos = e.target[11].value;
-  
-    let lat1 = decimal(latGraUno, latMinuUno, latSegUno,"s");
-    let lon1 = decimal(longGraUno, longMinuUno, longSegUno,"w");
-    let lat2 = decimal(latGraDos, latMinuDos, latSegDos,"s");
-    let lon2 = decimal(longGraDos, longMinuDos, longSegDos,"w");
-  
+  //Datos Latitud punto de LLegada
+  let latGraDos = e.target[6].value;
+  let latMinuDos = e.target[7].value;
+  let latSegDos = e.target[8].value;
 
+  //Datos Longitud punto de LLegada
 
+  let longGraDos = e.target[9].value;
+  let longMinuDos = e.target[10].value;
+  let longSegDos = e.target[11].value;
 
+  let lat1 = decimal(latGraUno, latMinuUno, latSegUno, "s");
+  let lon1 = decimal(longGraUno, longMinuUno, longSegUno, "w");
+  let lat2 = decimal(latGraDos, latMinuDos, latSegDos, "s");
+  let lon2 = decimal(longGraDos, longMinuDos, longSegDos, "w");
 
-    tituloCuatro.innerHTML =`${Dist(lat1, lon1, lat2, lon2)} Nm`
+  tituloCuatro.innerHTML = `${Dist(lat1, lon1, lat2, lon2)} Nm`;
 
-    smallLatUno.innerHTML = `Latitud:${latGraUno}° ${latMinuUno}" ${latSegUno}'`
-    smallLongUno.innerHTML= `Longitud:${longGraUno}° ${longMinuUno}" ${longSegUno}'`
-    smallLatDos.innerHTML=  `Latitud:${latGraDos}° ${latMinuDos}" ${latSegDos}'`
-    smallLongDos.innerHTML= `Longitud:${longGraDos}° ${longMinuDos}" ${longSegDos}'`
+  smallLatUno.innerHTML = `Latitud:${latGraUno}° ${latMinuUno}" ${latSegUno}'`;
+  smallLongUno.innerHTML = `Longitud:${longGraUno}° ${longMinuUno}" ${longSegUno}'`;
+  smallLatDos.innerHTML = `Latitud:${latGraDos}° ${latMinuDos}" ${latSegDos}'`;
+  smallLongDos.innerHTML = `Longitud:${longGraDos}° ${longMinuDos}" ${longSegDos}'`;
 
-    aPostUno.innerHTML='<a href="http://www.google.com/maps/place/'+lat1 +','+lon1+'" target="_blank">Ver tu punto en Google Maps</a>'
-    aPostDos.innerHTML='<a href="http://www.google.com/maps/place/'+lat2+','+lon2+'" target="_blank">Ver tu punto en Google Maps</a>'
+  aPostUno.innerHTML =
+    '<a href="http://www.google.com/maps/place/' +
+    lat1 +
+    "," +
+    lon1 +
+    '" target="_blank">Ver tu punto en Google Maps</a>';
+  aPostDos.innerHTML =
+    '<a href="http://www.google.com/maps/place/' +
+    lat2 +
+    "," +
+    lon2 +
+    '" target="_blank">Ver tu punto en Google Maps</a>';
 
+  resultFinal.appendChild(tituloCuatro);
 
- 
-    resultFinal.appendChild(tituloCuatro);
+  postUnoLartText.appendChild(smallLatUno);
+  postUnoLongText.appendChild(smallLongUno);
+  postDosLartText.appendChild(smallLatDos);
+  postDosLongText.appendChild(smallLongDos);
 
-    postUnoLartText.appendChild(smallLatUno);
-    postUnoLongText.appendChild(smallLongUno)
-    postDosLartText.appendChild(smallLatDos)
-    postDosLongText.appendChild(smallLongDos)
-
-    verMapaPostUno.appendChild(aPostUno)
-    verMapaPostDos.appendChild(aPostDos)
-
-
-
+  verMapaPostUno.appendChild(aPostUno);
+  verMapaPostDos.appendChild(aPostDos);
 }
-
-
-
-
